382918604df2b56501c24121b09dc862098910ca
Alexandros Baciu <alexbrex28@gmail.com>
2025-07-26 20:36:57 +0300

    Changed starting spped and speeduprate

--- settings.py
+++ settings.py
@@ -21,15 +21,15 @@ class Settings:
         self.fleet_drop_speed = 10
 
         # How quickly the game speeds up
-        self.speedup_scale = 1.5
+        self.speedup_scale = 1.2
 
         self.initialize_dynamic_settings()
 
     def initialize_dynamic_settings(self):
         """Initialize settings that change throughout the game."""
-        self.ship_speed = 1.5
-        self.bullet_speed = 2.5
-        self.alien_speed = 1.0
+        self.ship_speed = 1.8
+        self.bullet_speed = 2.8
+        self.alien_speed = 1.3
 
         # fleet_direction of 1 represents right; -1 represents left.
         self.fleet_direction = 1
